c01880b3d3892f0eca8934d2921a52565a2a779d
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-11 14:48:53 +0100

    :sparkles: Obtain props in HeaderText and display

--- src/components/header/HeaderText.js
+++ src/components/header/HeaderText.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function HeaderText() {
+function HeaderText(props) {
+    const {copyright, date, explanation, title} = props.props;
     return(
         <div>
-
+            <p>{date}</p>
+            <p>{title}</p>
+            <p>{copyright}</p>
+            <p>{explanation}</p>
         </div>
     )
 }
