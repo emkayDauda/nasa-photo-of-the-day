26585418a5bc00b4e7317b3bc935cf76fc85ea8e
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-12 14:07:10 +0100

    :lipstick: Style Single Rover story

--- src/components/rover/RoverStory.js
+++ src/components/rover/RoverStory.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function RoverStory(props){
-   const {img_src, earth_date, rover} = props.story;
-   console.log(props);
+const Story = styled.div`
+    border: .2rem solid dodgerblue;
+    width: 30rem;
+    /* height: 30rem; */
+    margin: 3rem 0;
+    padding-top: 3rem;
+    border-radius: 3rem;
+
+    img {
+        width: 25rem;
+        height: 25rem;
+        margin-top: 5rem;
+        border-radius: 3rem;
+        margin: 0 auto;
+    }
+`
+
+function RoverStory({story}){
+   const {img_src, earth_date, rover} = story;
    return (
-    <div className="story">
+    <Story>
         <img alt = "Story" src={img_src}/>
-        <p>{earth_date}</p>
-        <p>{rover.name}</p>
-    </div>
+        <p>Photo taken on {earth_date}</p>
+        <p>Mars Rover name: {rover.name}</p>
+        <p>Launched on {rover.launch_date}</p>
+        <p>Landed on {rover.landing_date}</p>
+        <p>Status: {rover.status}</p>
+    </Story>
    )
 }
 
